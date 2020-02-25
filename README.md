65ee79ccae6704c48eb017a6548fee85d6a8e223
Vedran Blazenka <blazenka.vedran@gmail.com>
2020-02-25 19:15:53 +0100

    chore: generate blog & add info

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal website
+
+## Developer building web applications
+
+I'm a JavaScript developer from Zagreb, Croatia working for [Kiwi.com](https://kiwi.com)  <br />
+Organizing and sharing knowledge at [JavaScript Zagreb](https://jszgb.com) and [NodeSchool Zagreb](https://nodeschool.io/zagreb)
+<br />
+
+
+## Credits
+Thanks to [alxshelepenok/gatsby-starter-lumen](https://github.com/alxshelepenok/gatsby-starter-lumen) for helping me scaffold this site.
---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://vedran.co',
+  pathPrefix: '/',
+  title: 'Blog by Vedran Blazenka',
+  subtitle: 'JavaScript developer focused on building fast and scalable user interfaces, teaching people and organizing events.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Vedran Blaženka',
+    photo: '/avatar.jpg',
+    bio: 'JavaScript developer focused on building fast and scalable user interfaces, teaching people and organizing events.',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: 'vblazenka',
+      github: 'vblazenka',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'vblazenka',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: 'vblazenka',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
